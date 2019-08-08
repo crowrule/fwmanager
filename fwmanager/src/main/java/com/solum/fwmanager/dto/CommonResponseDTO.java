b8d085b26c0993e855fbca5e44f66b78dd6bb0a1
dohoon <dohoon@localhost>
2019-08-08 19:46:45 +0900

    Initialize common response class

--- fwmanager/src/main/java/com/solum/fwmanager/dto/CommonResponseDTO.java
+++ fwmanager/src/main/java/com/solum/fwmanager/dto/CommonResponseDTO.java
@@ -1,10 +1,12 @@
 package com.solum.fwmanager.dto;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter @Setter
 public class CommonResponseDTO {
